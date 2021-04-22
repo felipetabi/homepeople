e08ba108ddf9fb4ba44903ae5ad001de1914d4e2
felipe <contreras.felipe905@gmail.com>
2021-04-22 17:58:35 -0400

    modelo categoria creado

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  first_name             :string           not null
+#  last_name              :string           not null
+#  birthday               :datetime
+#  role                   :integer          default(0)
+#  address                :string           not null
+#  region                 :string           not null
+#  comuna                 :string           not null
+#  description            :string           not null
+#  phone                  :string           not null
+#  terms                  :boolean
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
