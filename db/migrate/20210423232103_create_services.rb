04fe1acc2970963609c05d151d734f9cb921e775
felipe <contreras.felipe905@gmail.com>
2021-04-24 01:02:50 -0400

    service inicio

---
+++ db/migrate/20210423232103_create_services.rb
@@ -0,0 +1,12 @@
+class CreateServices < ActiveRecord::Migration[6.1]
+  def change
+    create_table :services do |t|
+      t.string :title
+      t.string :description
+      t.string :region
+      t.string :comuna
+
+      t.timestamps
+    end
+  end
+end
