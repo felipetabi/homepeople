04ae5edc0900db1df22a92cdeb96f9ce8215d9c5
felipe <contreras.felipe905@gmail.com>
2021-04-24 01:27:44 -0400

    aplication controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone, :birthday, :address, :role, :region, :comuna, :description, :photo])
-endend
+    
+end
