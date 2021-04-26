cbb43fe585afec61423f639daf735384f7860bbf
felipe <contreras.felipe905@gmail.com>
2021-04-25 23:37:52 -0400

    migracion arreglada

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_25_212929) do
+ActiveRecord::Schema.define(version: 2021_04_26_030849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,8 @@ ActiveRecord::Schema.define(version: 2021_04_25_212929) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_services_on_category_id"
     t.index ["user_id"], name: "index_services_on_user_id"
   end
 
@@ -84,5 +86,6 @@ ActiveRecord::Schema.define(version: 2021_04_25_212929) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "services", "categories"
   add_foreign_key "services", "users"
 end
