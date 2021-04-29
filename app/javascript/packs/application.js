7494aed363bfb9038a2b0bdac05cf31cd619e4b7
felipe <contreras.felipe905@gmail.com>
2021-04-29 01:19:05 -0400

    actualizacion de bootstrap 4.6->5

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
+import 'bootstrap/dist/js/bootstrap'
+import "bootstrap/dist/css/bootstrap";
 import  'css/styles'
 
 Rails.start()
