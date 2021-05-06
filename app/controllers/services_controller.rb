e7636f6f2ea1ee3cd7a2f0ffaf61523f84a5bc43
felipe <contreras.felipe905@gmail.com>
2021-05-06 00:09:38 -0400

    ambio en services para implementar creacion

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,6 +1,15 @@
 class ServicesController < ApplicationController
     def index
     end
+
+    def new
+        #session[:intentos] = session[:intentos] ? session[:Intentos] + 1 : 1
+       # @photo = Photo.new
+    end
+    def create
+        filtered_params = params.require(certificates:[]).permit(:title, :description, :region)
+    end
+
     private
     def service_params
         params.require(:service).permit(certificates: [])
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,5 +1,9 @@
 class Service < ApplicationRecord
 
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :region, presence: true 
+
     has_many_attached :certificates
     belongs_to :user
 
