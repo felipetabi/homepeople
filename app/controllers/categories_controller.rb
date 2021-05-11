f41171c1e8a6acb1047836ae039cf0e16c684aa2
felipe <contreras.felipe905@gmail.com>
2021-05-10 21:37:24 -0400

    ADDA :changes to control

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,19 @@
 class CategoriesController < ApplicationController
+    before_action :authenticate_user! do 
+        if current_user.role != 2
+            redirect_to services_path
+        end
+    end
+      
 
     def index
+        
+        @category = current_user.category || current_user.build_category
+    end
+
+    def create
+       
+        @category = current_user.category || current_user.build_category
+        @category = current_user.build_category(category_params)
     end
 end
