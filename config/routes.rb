3d11ed0c94651b8e7b313233844d988fcbec8998
felipe <contreras.felipe905@gmail.com>
2021-05-10 21:41:06 -0400

    ADD: resources categories

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get "categories/index"
 
   resources :services
+  resources :categories
+  
   
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
