64e7fb41348d98849f7666d42936b06468b1569a
Dinko <dinko@poliglota.cl>
2021-05-16 22:16:48 -0400

    ADD: calendar and asosiation service and working setting

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -4,41 +4,43 @@ class ServicesController < ApplicationController
 
 
     def index
-        @categories = Category.all
-        @service = current_user.service || current_user.build_service
+      @categories = Category.all
+      @service = current_user.service || current_user.build_service
+      @current_user_have_service = Service.find_by_user_id(current_user.id)
+      @working_settings = current_user.working_setting || current_user.build_working_setting
     end
 
 
     def create
-        @categories = Category.all
-        @service = current_user.service || current_user.build_service
-        @service = current_user.build_service(service_params)
-        if @service.save
-            redirect_to services_path, notice: "Haz creado el servicio"
-        else
-            render :index, status: :unprocessable_entity
-        end
+      @categories = Category.all
+      @service = current_user.service || current_user.build_service
+      @service = current_user.build_service(service_params)
+      if @service.save
+          redirect_to services_path, notice: "Haz creado el servicio"
+      else
+          render :index, status: :unprocessable_entity
+      end
     end
 
 
     def update
-        @categories = Category.all
-        @service = current_user.service || current_user.build_service
-        @service = current_user.service
-        if @service.update(service_params)
-            redirect_to services_path, notice: "Haz actualizado el servicio"
-        else
-            render :index, status: :unprocessable_entity
-        end
+      @categories = Category.all
+      @service = current_user.service || current_user.build_service
+      @service = current_user.service
+      if @service.update(service_params)
+          redirect_to services_path, notice: "Haz actualizado el servicio"
+      else
+          render :index, status: :unprocessable_entity
+      end
     end
 
     private
 
     def set_service
-        @service = Service.find(params[:id])
+      @service = Service.find(params[:id])
     end
 
     def service_params
-        params.require(:service).permit(:title, :description, :region, :comuna, :category_id, certificates: [])
+      params.require(:service).permit(:title, :description, :region, :comuna, :category_id, certificates: [])
     end
 end
