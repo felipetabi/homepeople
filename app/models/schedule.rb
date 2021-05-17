d05f1cc08c6ac66046eef7d0087ba2bb38312ce7
Dinko <dinko@poliglota.cl>
2021-05-16 22:16:13 -0400

    FIX: controller schedule

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,4 +1,22 @@
 class Schedule < ApplicationRecord
-    has_many :bookings
-    belongs_to :service
+  has_many :bookings
+  belongs_to :service
+
+  def title
+    "Servicio #{self.service.title}"
+  end
+
+  def start
+    now = self.day.to_datetime
+    hour = self.hour
+    DateTime.parse(now.strftime("%Y-%m-%dT#{hour}:00%z"))
+  end
+
+  def end
+    now = self.day.to_datetime
+    hour = self.hour.split(":").first.to_i + 1
+    hour = sprintf('%02d' ,hour) + ":00"
+    DateTime.parse(now.strftime("%Y-%m-%dT#{hour}:00%z"))
+  end
+
 end
