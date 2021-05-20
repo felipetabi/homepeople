62a69d73e4fe86e97b1e01182dd1c6b861d717dc
Dinko <dinko@poliglota.cl>
2021-05-19 22:53:41 -0400

    WIP

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,13 @@
 class SearchController < ApplicationController
-	
+
 	def index
 		@categories = Category.all
-		
+
 		query = {region: params[:region], comuna: params[:comuna], category_id: params[:category_id]}
 		@services = Service.where(query)
 		@cities = CS.states(:cl)
 	end
+	
 
 	def get_cities
 		if params[:region]
