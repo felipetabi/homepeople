505efcdb126f3aafb3538f2912c9f56c8b214a90
felipe <contreras.felipe905@gmail.com>
2021-05-21 15:59:59 -0400

    Eliminate columns from user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone, :birthday, :address, :role, :region, :comuna, :description, :photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone, :birthday, :role, :photo])
   end
 end
