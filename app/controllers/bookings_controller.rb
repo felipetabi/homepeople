00eb385137c5f90df7a525599ccb2da84cd0e959
Dinko <dinko@poliglota.cl>
2021-05-21 22:05:11 -0400

    ADD: create reserve

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,6 +7,15 @@ class BookingsController < ApplicationController
   def my_services_booking
     @my_services_booking = current_user.service.schedules.joins(:bookings)
   end
-  
+
+  def create_reserve
+    schedules_ids = params[:schedule_ids].split(",")
+    schedules_ids.each do |id|
+      schedule = Schedule.find(id)
+      schedule.update(is_closed: true)
+      Booking.create(schedule_id: id, client: current_user, status: 1)
+    end
+    redirect_to bookings_path, notice: "Haz reservado"
+  end
 
 end
