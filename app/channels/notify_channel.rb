1a051e31ed0dd2da1e2c31537136ca583c0250ab
Dinko <dinko@poliglota.cl>
2021-05-27 23:24:51 -0400

    ADD: suscribe user by id

--- app/channels/notify_channel.rb
+++ app/channels/notify_channel.rb
@@ -1,16 +1,12 @@
 class NotifyChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
-    #puts "\n\n Pipe  \n\n"
-    #puts "\n\n #{ current_user }\n\n"
-    #puts "\n\n fjasjfaskfl \n\n"
-    stream_from  current_user
-    
-
-    
+    stream_from  "user_#{current_user.id}"
+    # ActionCable.server.broadcast(current_user, { message: "Hola #{ current_user.email }" })
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 end
+#<User:0x00007f591c0109b0>
+#<User:0x0000560f420576c8>
