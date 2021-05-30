da1b478f736e3041e85f0fe00d3daa057fa71053
felipe <contreras.felipe905@gmail.com>
2021-05-29 20:26:18 -0400

    notificacionesnavbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :user_notification
 
   def is_admin?
     if current_user.role != 2
       redirect_to services_path, alert: "No tienes accesos para entrar acá :P"
     end
+  end
+
+  def user_notification
+    if user_signed_in?
+      @user_notify = Notification.where(receiver_id: current_user.id, state: false)
+
+    end 
   end
 
   protected
