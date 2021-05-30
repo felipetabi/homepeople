41ec2688ec67bf0aa81052f177b5d89f7870d5ab
Dinko <dinko@poliglota.cl>
2021-05-29 20:48:25 -0400

    FIX: install bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "@popperjs/core"
 import "bootstrap"
-import 'bootstrap/dist/js/bootstrap'
-import  'css/styles'
 import "controllers"
+import "../stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,10 +1,5 @@
 class Notification < ApplicationRecord
-    
     belongs_to :notificable, polymorphic: true
-
-    
     validates :message, presence: true
-
-    
 
 end
