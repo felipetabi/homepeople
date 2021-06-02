e23f465c53bf88cd191894109ec767925fde7330
felipe <contreras.felipe905@gmail.com>
2021-06-02 19:26:19 -0400

    ADD create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,25 @@
-class CommentsController < ApplicationController
+class CommentsController < 
+	before_action :authenticate_user!
+
+	def create
+    @comment = current_user.comment || current_user.build_comment
+    @comment = current_user.build_comment(comment_params)
+		if @comment.save
+			#redirect_to
+		else
+			render :index, status: :unprocessable_entity
+		end
+	end
+
+
+	private
+
+	#def set_comment
+	#	@comment = Comment.find(params[:id])
+	#end
+
+	def comment_params
+		params.require(:comment).permit(:rating, :body)
+	end
+	
 end
