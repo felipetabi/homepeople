0696d0f05f62c30b32fd3448bd0d486c3cf320b0
Dinko <dinko@poliglota.cl>
2021-06-19 18:18:56 -0400

    ADD: new element to services

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,44 +1,21 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id                     :bigint           not null, primary key
-#  email                  :string           default(""), not null
-#  encrypted_password     :string           default(""), not null
-#  reset_password_token   :string
-#  reset_password_sent_at :datetime
-#  remember_created_at    :datetime
-#  first_name             :string           not null
-#  last_name              :string           not null
-#  birthday               :datetime
-#  role                   :integer          default(0)
-#  address                :string           not null
-#  region                 :string           not null
-#  comuna                 :string           not null
-#  description            :string           not null
-#  phone                  :string           not null
-#  terms                  :boolean
-#  created_at             :datetime         not null
-#  updated_at             :datetime         not null
-#
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one_attached :photo
-
   has_one :service, dependent: :destroy
+  has_many :schedules, through: :service
   has_one :working_setting, dependent: :destroy
   has_many :bookings, foreign_key: "client_id", dependent: :destroy
   # has_many :client_chats, foreign_key: "client_id", dependent: :destroy
   has_many :notifications, as: :notificable, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_one_attached :photo
+
+  enum role: %w{user service_client}
 
 
   def full_name
     self.first_name + " " + self.last_name
   end
-  
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_19_211133) do
+ActiveRecord::Schema.define(version: 2021_06_19_220221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -138,6 +138,12 @@ ActiveRecord::Schema.define(version: 2021_06_19_211133) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.string "last_job"
+    t.datetime "start_date_last_job"
+    t.datetime "end_date_last_job"
+    t.text "description_last_job"
+    t.string "categories_last_job", default: [], array: true
+    t.string "address_last_job"
     t.index ["user_id"], name: "index_services_on_user_id"
   end
 
